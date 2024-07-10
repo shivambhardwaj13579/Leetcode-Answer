182e8629bae901dde22a52c067fa1de9d5318e0c
Shivam Bhardwaj <shivambhardwaj13579@gmail.com>
2024-07-10 22:35:52 +0530

    1

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result [] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int ans = nums[i]+nums[j];
+                if(ans == target){
+                    result [0] = i;
+                    result [1] = j;
+                }
+            }
+        }
+    return result;
+    }
+}
