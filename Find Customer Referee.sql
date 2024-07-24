8ed4e29f0e668c52f1e3f624fd306d08df718312
Shivam Bhardwaj <154930144+shivambhardwaj13579@users.noreply.github.com>
2024-07-24 23:55:48 +0530

    584

--- Find Customer Referee.sql
+++ Find Customer Referee.sql
@@ -1,5 +1,5 @@
 /* https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50 */
 
 /* Write your MySQL query statement below */
-select name from customer 
+select name from customer
 where referee_id is null or referee_id <> 2
