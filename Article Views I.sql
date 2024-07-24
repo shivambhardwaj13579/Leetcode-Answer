b5ab3a8c336b3431713004a5f8a0b28bed00f8aa
Shivam Bhardwaj <shivambhardwaj13579@gmail.com>
2024-07-24 23:50:57 +0530

    1148

---
+++ Article Views I.sql
@@ -0,0 +1,6 @@
+/* https://leetcode.com/problems/article-views-i/ */
+
+/* Write your MySQL query statement below*/
+select distinct author_id as id from Views
+where author_id = viewer_id 
+order by id;
