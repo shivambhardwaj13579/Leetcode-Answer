f7bf933ce85ffb532cf12d78d32c50a8dfb99aa0
Shivam Bhardwaj <154930144+shivambhardwaj13579@users.noreply.github.com>
2024-07-24 23:57:19 +0530

    1757

--- Recyclable and Low Fat Products.sql
+++ Recyclable and Low Fat Products.sql
@@ -1,6 +1,6 @@
 /* https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50 */
 
 /* Write your MySQL query statement below */
-SELECT Products.product_id 
-FROM Products 
+SELECT Products.product_id
+FROM Products
 WHERE Products.low_fats = 'Y' AND Products.recyclable = 'Y';
