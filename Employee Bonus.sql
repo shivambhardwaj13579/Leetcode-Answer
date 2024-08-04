2dbe62042c10a7d31252ab463b04dbf02af90e32
Shivam Bhardwaj <shivambhardwaj13579@gmail.com>
2024-08-04 17:35:50 +0530

    577

---
+++ Employee Bonus.sql
@@ -0,0 +1,6 @@
+/* https://leetcode.com/problems/employee-bonus/ */
+
+/* Write your MySQL query statement below */
+SELECT Employee.name,Bonus.bonus FROM Employee 
+LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+WHERE bonus < 1000 OR Bonus IS NULL ;
