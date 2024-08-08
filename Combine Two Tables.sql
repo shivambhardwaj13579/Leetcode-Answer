829ba9ccd931baffec377af6781b042d5b84691c
Shivam Bhardwaj <shivambhardwaj13579@gmail.com>
2024-08-08 23:48:10 +0530

    175

---
+++ Combine Two Tables.sql
@@ -0,0 +1,7 @@
+/* https://leetcode.com/problems/combine-two-tables/description/ */
+
+/* Write your MySQL query statement below */
+select firstName, lastName, city, state
+from Person p
+left join Address a
+    on p.personId = a.personId;
